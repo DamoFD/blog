c8acc6e5c6af20cf7788ba292934233c47c76313
DamoFD <voshall@yahoo.com>
2023-05-18 22:48:18 -0700

    added create user functionality

--- app/pages/admin.php
+++ app/pages/admin.php
@@ -6,6 +6,83 @@
 
     }
 
+    $section = $url[1] ?? 'dashboard';
+    $action = $url[2] ?? 'view';
+    $id = $url[3] ?? 0;
+
+    $filename = "../app/pages/admin/" . $section . ".php";
+    if(!file_exists($filename)){
+
+        $filename = "../app/pages/admin/404.php";
+
+    }
+
+    if(!empty($_POST))
+        {
+
+    // Add New User
+    if($action == 'add'){
+
+          //validate
+          $errors = [];
+
+          if(empty($_POST['name']))
+          {
+            $errors['name'] = "A name is required";
+          }else
+          if(!preg_match("/^[a-zA-Z]+[a-zA-Z ]*$/", $_POST['name']))
+          {
+            $errors['name'] = "name can only have letters and spaces";
+          }
+
+          $query = "SELECT id FROM admin WHERE email = :email LIMIT 1";
+          $email = query($query, ['email'=>$_POST['email']]);
+
+          if(empty($_POST['email']))
+          {
+            $errors['email'] = "A email is required";
+          }else
+          if(!filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+          {
+            $errors['email'] = "Email not valid";
+          }else
+          if($email)
+          {
+            $errors['email'] = "That email is already in use";
+          }
+
+          if(empty($_POST['password']))
+          {
+            $errors['password'] = "A password is required";
+          }else
+          if(strlen($_POST['password']) < 8)
+          {
+            $errors['password'] = "Password must be 8 character or more";
+          }else
+          if($_POST['password'] !== $_POST['confirm_pwd'])
+          {
+            $errors['password'] = "Passwords do not match";
+          }
+
+          }
+   
+          if(empty($errors))
+          {
+            //save to database
+            $data = [];
+            $data['name'] = $_POST['name'];
+            $data['email']    = $_POST['email'];
+            $data['password'] = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+            $query = "INSERT INTO admin (name,email,password) VALUES (:name,:email,:password)";
+
+            query($query, $data);
+
+            redirect('admin/users');
+
+          }
+        }
+
 ?>
 
 <!DOCTYPE html>
@@ -43,16 +120,8 @@
     <a href="<?php echo ROOT; ?>/logout">Sign Out</a>
 
     <?php
-    
-    $section = $url[1] ?? 'dashboard';
-    $action = $url[2] ?? 'dashboard';
 
-    $filename = "../app/pages/admin/" . $section . ".php";
-    if(file_exists($filename)){
         require_once($filename);
-    }else{
-        require_once("../app/pages/admin/404.php");
-    }
 
     ?>
 
--- app/pages/admin/users.php
+++ app/pages/admin/users.php
@@ -1,9 +1,47 @@
+<?php if($action == 'add'): ?>
+    <h2>Create User</h2>
+    <form method="post">
+
+        <?php if (!empty($errors)): ?>
+            <p>Please fix the errors below</p>
+        <?php endif; ?>
+
+        <label>Name</label>
+        <input name="name" type="text" value="<?=old_value('name')?>" />
+        <?php if(!empty($errors['name'])): ?>
+            <p><?=$errors['name']?></p>
+        <?php endif; ?>
+
+        <label>Email</label>
+        <input name="email" type="email" value="<?=old_value('email')?>" />
+        <?php if(!empty($errors['email'])): ?>
+            <p><?=$errors['email']?></p>
+        <?php endif; ?>
+
+        <label>Password</label>
+        <input name="password" type="password" value="<?=old_value('password')?>" />
+        <?php if(!empty($errors['password'])): ?>
+            <p><?=$errors['password']?></p>
+        <?php endif; ?>
+
+        <label>Confirm Password</label>
+        <input name="confirm_pwd" type="password" value="<?=old_value('confirm_pwd')?>" />
+        <button type="submit">Create</button>
+    </form>
+
+<?php elseif($action == 'edit'): ?>
+
+<?php elseif($action == 'delete'): ?>
+
+<?php else: ?>
+
 <h2>Users</h2>
-<button>Add New</button>
+<a href="<?php echo ROOT; ?>/admin/users/add">Add New</a>
 
 <table>
     <tr>
         <th>#</th>
+        <th>Name</th>
         <th>Email</th>
         <th>Date</th>
         <th>Action</th>
@@ -19,10 +57,16 @@
         <?php foreach($rows as $row): ?>
     <tr>
         <td><?=$row['id'] ?></td>
+        <td><?=$row['name'] ?></td>
         <td><?=esc($row['email']) ?></td>
-        <td><? if(!empty($row['date'])) echo date("jS M, Y", strtotime($row['date'])) ?></td>
-        <td><button>Edit</button><button>Trash</button></td>
+        <td><?=date("jS M, Y", strtotime($row['date'])) ?></td>
+        <td>
+            <a href="<? echo ROOT; ?>/admin/users/edit/<? echo $row['id'] ?>">Edit</a>
+            <a href="<? echo ROOT; ?>/admin/users/delete/<? echo $row['id'] ?>">Delete</a>
+        </td>
     </tr>
         <?php endforeach; ?>
     <?php endif; ?>
 </table>
+
+<?php endif; ?>
