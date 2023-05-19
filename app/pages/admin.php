6539b94fcb02552137544e3d9af755fbb9966194
DamoFD <voshall@yahoo.com>
2023-05-18 17:00:40 -0700

    began backend boilerplate

---
+++ app/pages/admin.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin - <? echo APP_NAME; ?></title>
+
+    <link href="<?php echo ROOT; ?>/assets/css/dashboard.css" rel="stylesheet" />
+</head>
+<body>
+    <h1>Dashboard</h1>
+    <input type="text" placeholder="Search" />
+    <button>Sign Out</button>
+    <nav>
+        <ul>
+            <li>
+                Dashboard
+            </li>
+            <li>
+                Front-End
+            </li>
+        </ul>
+    </nav>
+</body>
+</html>
