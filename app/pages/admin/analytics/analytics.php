14bce593ff870a6dc4eaa6c4bc9386d63756d929
DamoFD <voshall@yahoo.com>
2023-05-20 23:44:10 -0700

    analytics functions

--- app/pages/admin/analytics/analytics.php
+++ app/pages/admin/analytics/analytics.php
@@ -39,4 +39,154 @@ foreach($web_analytics_db->query("SELECT `visitor_country`, COUNT(*) FROM wa_req
     }
 }
 
+// Top Origins
+$top_origins = array_merge($top_countries, $top_continents);
+
+// Sort top origins
+asort($top_origins);
+
+// Sort top continents
+arsort($top_continents);
+
+$total_countries = 0;
+$top_countriesvo = array();
+$top_continentsvo = array();
+
+// Retrieve Total Countries
+foreach($web_analytics_db->query("SELECT `country`, COUNT(*) FROM wa_browsers GROUP BY `country` ORDER BY COUNT(*) DESC;") as $country) {
+    if($country[0] != "" && $country[0] != null) {
+        $top_countriesvo[$country[0]] = $country[1];
+        $continent = $country_to_continent[strtoupper($country[0])];
+        if(!array_key_exists($continent, $top_continentsvo)) {
+            $top_continentsvo[$continent] = $country[1];
+        } else {
+            $top_continentsvo[$continent] = $top_continentsvo[$continent] + $country[1];
+        }
+        $total_countries = $total_countries + 1;
+    } else {
+        $top_countriesvo["?"] = $country[1];
+    }
+}
+
+$top_originsvo = array_merge($top_countriesvo, $top_continentsvo);
+$top_languages = array();
+$total_languages = 0;
+
+// Retrieve total languages and top languages
+foreach($web_analytics_db->query("SELECT `language`, COUNT(*) FROM wa_browsers GROUP BY `language` ORDER BY COUNT(*) DESC;") as $language) {
+    if($language[0] != "" && $language[0] != null) {
+        $top_languages[$language[0]] = $language[1];
+        $total_languages = $total_languages + 1;
+    } else {
+        $top_languages["?"] = $language[1];
+    }
+}
+
+$top_useragents = array();
+$top_browsers = array();
+$top_oss = array();
+
+// Retrieve top useragent, browser, os
+foreach($web_analytics_db->query("SELECT `user_agent`, COUNT(*) FROM wa_browsers GROUP BY `user_agent` ORDER BY COUNT(*) DESC LIMIT 10;") as $useragent) {
+    $top_useragents[$useragent[0]] = $useragent[1];
+    $uaa = analyse_user_agent($useragent[0]);
+    if(isset($top_browsers[$uaa["browser"]["name"]])) {
+        $top_browsers[$uaa["browser"]["name"]] += $useragent[1];
+    } else {
+        $top_browsers[$uaa["browser"]["name"]] = $useragent[1];
+    }
+    if(isset($top_oss[$uaa["os"]["name"]])) {
+        $top_oss[$uaa["os"]["name"]] += $useragent[1];
+    } else {
+        $top_oss[$uaa["os"]["name"]] = $useragent[1];
+    }
+}
+
+$total_isps = 0;
+$top_isps = array();
+
+// Retrieve top and total isps
+foreach($web_analytics_db->query("SELECT `isp`, COUNT(*) FROM wa_ips GROUP BY `isp` ORDER BY COUNT(*) DESC LIMIT 10;") as $isp) {
+    if($isp[0] != "" && $isp[0] != null) {
+        $top_isps[$isp[0]] = $isp[1];
+        $total_isps++;
+    } else {
+        $top_isps["?"] = $isp[1];
+    }
+}
+
+$top_uris = array();
+
+// Fetch top uris
+foreach($web_analytics_db->query("SELECT `uri`, COUNT(*) FROM wa_requests GROUP BY `uri` ORDER BY COUNT(*) DESC LIMIT 10;") as $uri) {
+    $top_uris[$uri[0]] = $uri[1];
+}
+
+$last_requests = array();
+$last_requests_by_daytime = array();
+$last_requests_by_day = array();
+$last_requests_by_weekday = array();
+$last_visitors = array();
+$last_visitors_by_daytime = array();
+$last_visitors_by_day = array();
+$last_visitors_by_weekday = array();
+
+// Fetch requests by time
+foreach($web_analytics_db->query("SELECT `time`, `browser_id` FROM wa_requests ORDER BY `time` LIMIT 1000;") as $request) {
+    $time = $request[0];
+    $daytime = date("[H, 0, 0]", strtotime($time));
+    $day = date("Y, m, d", strtotime($time));
+    $weekday = date("l", strtotime($time));
+    if(isset($last_requests[$time])) {
+        $last_requests[$time] += 1;
+    } else {
+        $last_requests[$time] = 1;
+    }
+    if(isset($last_requests_by_day[$day])) {
+        $last_requests_by_day[$day] += 1;
+    } else {
+        $last_requests_by_day[$day] = 1;
+    }
+    if(isset($last_requests_by_weekday[$weekday])) {
+        $last_requests_by_weekday[$weekday] += 1;
+    } else {
+        $last_requests_by_weekday[$weekday] = 1;
+    }
+    if(isset($last_requests_by_daytime[$daytime])) {
+        $last_requests_by_daytime[$daytime] += 1;
+    } else {
+        $last_requests_by_daytime[$daytime] = 1;
+    }
+    if(isset($last_visitors[$time])) {
+        if(!isset($last_visitors[$time][$request[1]])) {
+            $last_visitors[$time][$request[1]] = 1;
+        }
+    } else {
+        $last_visitors[$time] = array($request[1] => 1);
+    }
+    if(isset($last_visitors_by_day[$day])) {
+        if(!isset($last_visitors_by_day[$day][$request[1]])) {
+            $last_visitors_by_day[$day][$request[1]] = 1;
+        }
+    } else {
+        $last_visitors_by_day[$day] = array($request[1] => 1);
+    }
+    if(isset($last_visitors_by_weekday[$weekday])) {
+        if(!isset($last_visitors_by_weekday[$weekday][$request[1]])) {
+            $last_visitors_by_weekday[$weekday][$request[1]] = 1;
+        }
+    } else {
+            $last_visitors_by_weekday[$weekday] = array($request[1] => 1);
+    }
+    if(isset($last_visitors_by_daytime[$daytime])) {
+        if(!isset($last_visitors_by_daytime[$daytime][$request[1]])) {
+            $last_visitors_by_daytime[$daytime][$request[1]] = 1;
+        }
+    } else {
+        $last_visitors_by_daytime[$daytime] = array($request[1] => 1);
+    }
+}
+ksort($last_requests_by_daytime);
+ksort($last_visitors_by_daytime);
+
 ?>
