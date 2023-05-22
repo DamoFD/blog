38ab524dcb8687796903108fd72dcbccbc3adab2
DamoFD <voshall@yahoo.com>
2023-05-21 20:14:50 -0700

    comment card mobile

--- app/pages/admin/dashboard.php
+++ app/pages/admin/dashboard.php
@@ -12,9 +12,9 @@ $yesterday = date('Y, m, d', strtotime("-1 day"));
 $viewsToday = isset($last_requests_by_day[$today]) ? $last_requests_by_day[$today] : 0;
 $viewsYesterday = isset($last_requests_by_day[$yesterday]) ? $last_requests_by_day[$yesterday] : 0;
 
-if($viewsYesterday > 0){
+if ($viewsYesterday > 0) {
     $viewPercentChange = (($viewsToday - $viewsYesterday) / $viewsYesterday) * 100;
-}else{
+} else {
     $viewPercentChange = $viewsToday > 0 ? 100 : 0;
 }
 
@@ -22,9 +22,9 @@ if($viewsYesterday > 0){
 $visitorsToday = isset($last_visitors_by_day[$today]) ? array_sum($last_visitors_by_day[$today]) : 0;
 $visitorsYesterday = isset($last_visitors_by_day[$yesterday]) ? array_sum($last_visitors_by_day[$yesterday]) : 0;
 
-if($visitorsYesterday > 0){
+if ($visitorsYesterday > 0) {
     $visitorPercentChange = (($visitorsToday - $visitorsYesterday) / $visitorsYesterday) * 100;
-}else{
+} else {
     $visitorPercentChange = $visitorsToday > 0 ? 100 : 0;
 }
 
@@ -43,50 +43,56 @@ if($visitorsYesterday > 0){
     <div class="card">
         <h2 class="font-poppins light-header">TODAY'S VIEWS</h2>
         <div class="row">
-        <p class="font-roboto amount"><?=array_sum($last_requests_by_daytime)?></p>
-        <div class="percent">
-        <p class="font-roboto <?php echo $viewPercentChange >= 0 ? "positive" : "negative"; ?>"><?=round($viewPercentChange, 2)?>%</p>
-        <img src="<?=ROOT?>/assets/svg/<?php echo $viewPercentChange >= 0 ? "arrow-up.svg" : "arrow-down.svg"; ?>" />
-        </div>
+            <p class="font-roboto amount"><?= array_sum($last_requests_by_daytime) ?></p>
+            <div class="percent">
+                <p class="font-roboto <?php echo $viewPercentChange >= 0 ? "positive" : "negative"; ?>"><?= round($viewPercentChange, 2) ?>%</p>
+                <img src="<?= ROOT ?>/assets/svg/<?php echo $viewPercentChange >= 0 ? "arrow-up.svg" : "arrow-down.svg"; ?>" />
+            </div>
         </div>
     </div>
     <div class="card">
         <h2 class="font-poppins light-header">TODAY'S VISITORS</h2>
         <div class="row">
-            <p class="font-roboto amount"><?=$visitorsToday?></p>
+            <p class="font-roboto amount"><?= $visitorsToday ?></p>
             <div class="percent">
-            <p class="font-roboto <?php echo $visitorPercentChange >= 0 ? "positive" : "negative"; ?>"><?=round($visitorPercentChange, 2)?>%</p>
-            <img src="<?=ROOT?>/assets/svg/<?php echo $visitorPercentChange >= 0 ? "arrow-up.svg" : "arrow-down.svg"; ?>" />
+                <p class="font-roboto <?php echo $visitorPercentChange >= 0 ? "positive" : "negative"; ?>"><?= round($visitorPercentChange, 2) ?>%</p>
+                <img src="<?= ROOT ?>/assets/svg/<?php echo $visitorPercentChange >= 0 ? "arrow-up.svg" : "arrow-down.svg"; ?>" />
             </div>
         </div>
     </div>
     <div class="card">
         <h2 class="font-poppins light-header">UNREAD NOTIFICATIONS</h2>
         <div class="row">
-        <p class="font-roboto amount">35</p>
+            <p class="font-roboto amount">35</p>
         </div>
     </div>
     <div class="card">
         <h2 class="font-poppins light-header">UNREAD EMAILS</h2>
         <div class="row">
-        <p class="font-roboto amount">13</p>
+            <p class="font-roboto amount">13</p>
         </div>
     </div>
     <div class="card">
         <?php include("../app/pages/admin/analytics/traffic.php"); ?>
     </div>
     <div class="card">
-        <?php include("../app/pages/admin/analytics/referrers.php");?>
+        <?php include("../app/pages/admin/analytics/referrers.php"); ?>
     </div>
     <div class="card">
-        <h2 class="font-sans font-size-med email-header">Emails</h2>
-        <p class="font-poppins font-size-small email-header-text">Recent Emails</p>
+        <div class="email-head">
+            <div class="email-text-container">
+                <h2 class="font-sans font-size-med email-header">Emails</h2>
+                <p class="font-poppins email-header-text">Recent Emails</p>
+            </div>
+            <a class="font-roboto" href="<?= ROOT ?>/admin/email">See all Emails <img src="<?= ROOT ?>/assets/svg/arrow-right.svg" /></a>
+        </div>
         <div class="email-row">
             <div class="email-read">
-                <p>Read</p>
+                <img src="<?= ROOT ?>/assets/svg/green-dot.svg" />
+                <p class="font-roboto">Read</p>
             </div>
             <div class="email-subject">
-                <h3>Wordpress Website</h3>
+                <h3 class="font-poppins">Wordpress Website</h3>
                 <p class="mobile-hidden">question about how too...</p>
             </div>
             <div class="email-date mobile-hidden">
@@ -94,18 +100,19 @@ if($visitorsYesterday > 0){
                 <p>Sent from form</p>
             </div>
             <div class="reply-email">
-                <p>john@john.com</p>
+                <p class="font-roboto">john@john.com</p>
             </div>
-            <div class="email-btn">
-                <img src="<?=ROOT?>/assets/svg/three-dots.svg" />
+            <div class="email-btn mobile-hidden">
+                <img src="<?= ROOT ?>/assets/svg/three-dots.svg" />
             </div>
         </div>
         <div class="email-row">
             <div class="email-read">
-                <p>Read</p>
+                <img src="<?= ROOT ?>/assets/svg/green-dot.svg" />
+                <p class="font-roboto">Read</p>
             </div>
             <div class="email-subject">
-                <h3>Wordpress Website</h3>
+                <h3 class="font-poppins">Wordpress Website</h3>
                 <p class="mobile-hidden">question about how too...</p>
             </div>
             <div class="email-date mobile-hidden">
@@ -113,18 +120,19 @@ if($visitorsYesterday > 0){
                 <p>Sent from form</p>
             </div>
             <div class="reply-email">
-                <p>john@john.com</p>
+                <p class="font-roboto">john@john.com</p>
             </div>
-            <div class="email-btn">
-                <img src="<?=ROOT?>/assets/svg/three-dots.svg" />
+            <div class="email-btn mobile-hidden">
+                <img src="<?= ROOT ?>/assets/svg/three-dots.svg" />
             </div>
         </div>
         <div class="email-row">
             <div class="email-read">
-                <p>Read</p>
+                <img src="<?= ROOT ?>/assets/svg/green-dot.svg" />
+                <p class="font-roboto">Read</p>
             </div>
             <div class="email-subject">
-                <h3>Wordpress Website</h3>
+                <h3 class="font-poppins">Wordpress Website</h3>
                 <p class="mobile-hidden">question about how too...</p>
             </div>
             <div class="email-date mobile-hidden">
@@ -132,11 +140,28 @@ if($visitorsYesterday > 0){
                 <p>Sent from form</p>
             </div>
             <div class="reply-email">
-                <p>john@john.com</p>
+                <p class="font-roboto">john@john.com</p>
             </div>
-            <div class="email-btn">
-                <img src="<?=ROOT?>/assets/svg/three-dots.svg" />
+            <div class="email-btn mobile-hidden">
+                <img src="<?= ROOT ?>/assets/svg/three-dots.svg" />
             </div>
+        </div>
+    </div>
+    <div class="card">
+        <h2 class="font-sans comment-header">Recent Comments</h2>
+        <p class="font-poppins comment-header-text">Here are your latest comments</p>
+        <div>
+            <div class="comment-row">
+                <div>
+                <h3 class="font-poppins comment-text-dark">Jenny Wilson</h3>
+                <p class="font-roboto comment-text-light">I really like how...</p>
+                </div>
+                <div class="message-info">
+                    <p class="font-poppins comment-text-dark">Cute Kitties</p>
+                    <p class="font-roboto comment-text-light">Jan 17, 2022</p>
+                </div>
+            </div>
+            <a class="font-roboto comments-link" href="">SEE ALL COMMENTS<img src="<?=ROOT?>/assets/svg/arrow-right.svg" /></a>
         </div>
     </div>
 </section>
