ee56cff72f660138b08f3709c47705f0193f27aa
DamoFD <voshall@yahoo.com>
2023-05-26 14:05:29 -0700

    404 styling

--- app/pages/404.php
+++ app/pages/404.php
@@ -1,5 +1,9 @@
 <?php include('../app/pages/includes/header.php'); ?>
 
-    <h1>404 Page not found!</h1>
-    
+<section id="not-found">
+    <h1 class="font-sans font-size-header">404 Page not found!</h1>
+</section>
+
+<?php include('../app/pages/includes/contact.php'); ?>
+
 <?php include('../app/pages/includes/footer.php'); ?>
