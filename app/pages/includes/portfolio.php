6deb7262c8c8e60ad88b9da2921383efdcfc5920
DamoFD <voshall@yahoo.com>
2023-06-05 10:33:59 -0700

    reorganized portfolio section

--- app/pages/includes/portfolio.php
+++ app/pages/includes/portfolio.php
@@ -9,27 +9,16 @@
       <div class="color-primary-bg line-break"></div>
     </div>
     <div class="grid">
-      <a href="<?=ROOT?>/luxe-style-society">
-      <div class="slide-up grid-item">
-        <div class="grid-content">
-          <h3 class="color-secondary font-poppins">
-            Luxe Style <span class="color-primary">Society</span>
-          </h3>
+      <a href="<?= ROOT ?>/portfolio-cms">
+        <div class="slide-up grid-item">
+          <div class="grid-content">
+            <h3 class="color-secondary font-poppins font-size-med">
+              Portfolio <span class="color-primary">CMS</span>
+            </h3>
+          </div>
+          <div class="overlay"></div>
+          <img class="grid-img" src="<?= ROOT ?>/assets/images/portfolio-cms.webp" />
         </div>
-        <div class="overlay"></div>
-        <img class="grid-img" src="<?= ROOT ?>/assets/images/luxe-style-society.png" />
-      </div>
-      </a>
-      <a href="<?=ROOT?>/skylinepress">
-      <div class="slide-up grid-item">
-        <div class="grid-content">
-          <h3 class="color-secondary font-poppins font-size-med">
-            SkyLine<span class="color-primary">Press</span>
-          </h3>
-        </div>
-        <div class="overlay"></div>
-        <img class="grid-img" src="<?= ROOT ?>/assets/images/SkyLinePress ScreenShot.png" />
-      </div>
       </a>
       <a href="<?= ROOT ?>/reno-artistry">
         <div class="slide-up grid-item">
@@ -42,16 +31,27 @@
           <img class="grid-img" src="<?= ROOT ?>/assets/images/reno-artistry.webp" />
         </div>
       </a>
-      <a href="<?=ROOT?>/portfolio-cms">
-      <div class="slide-up grid-item">
-        <div class="grid-content">
-          <h3 class="color-secondary font-poppins font-size-med">
-            Portfolio <span class="color-primary">CMS</span>
-          </h3>
+      <a href="<?= ROOT ?>/luxe-style-society">
+        <div class="slide-up grid-item">
+          <div class="grid-content">
+            <h3 class="color-secondary font-poppins">
+              Luxe Style <span class="color-primary">Society</span>
+            </h3>
+          </div>
+          <div class="overlay"></div>
+          <img class="grid-img" src="<?= ROOT ?>/assets/images/luxe-style-society.png" />
         </div>
-        <div class="overlay"></div>
-        <img class="grid-img" src="<?= ROOT ?>/assets/images/portfolio-cms.webp" />
-      </div>
+      </a>
+      <a href="<?= ROOT ?>/skylinepress">
+        <div class="slide-up grid-item">
+          <div class="grid-content">
+            <h3 class="color-secondary font-poppins font-size-med">
+              SkyLine<span class="color-primary">Press</span>
+            </h3>
+          </div>
+          <div class="overlay"></div>
+          <img class="grid-img" src="<?= ROOT ?>/assets/images/SkyLinePress ScreenShot.png" />
+        </div>
       </a>
     </div>
   </div>
